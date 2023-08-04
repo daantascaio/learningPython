e5f5ae23c0f7ac73bb700e3135c0a81a617e65d3
Caio Dantas <caiodantasl.lara@gmail.com>
2023-08-04 14:02:48 -0300

    just saving small changes

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+"""
+
+Calculadora simples: Crie uma calculadora que permita ao usuário realizar operações básicas de adição, subtração, multiplicação e divisão.
+
+"""
+
+# pedir para o usuário escolher qual opeção ele quer fazer if + elif - elif * elif /
+# tratar o dado recebido pelo usuário
+# input usuário número da direita, lembrando ele que a ordem de digitação do número pode afetar o resultado do calculo
+# input usuario número da esquerda
+# tratar o input dos números
+# fazer a operação e exibir o resultado
+# exibir um menu para que o usuário selecione qual operação fazer ou se ele deseja encerrar a calculadora 
+
+
+
+
+
+
+
+       
+
+
+
