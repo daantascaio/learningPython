cbf748cde8da5215eaa1525c1bab0ef7dbe697fe
Caio Dantas <caiodantasl.lara@gmail.com>
2023-08-06 08:29:11 -0300

    saving progress

---
+++ conversor_de_unidades.py
@@ -0,0 +1,7 @@
+"""
+
+Conversor de unidades: Desenvolva um programa que converta valores
+entre diferentes unidades, como Celsius para Fahrenheit, metros para pés, quilogramas para libras, etc.
+
+"""
+
